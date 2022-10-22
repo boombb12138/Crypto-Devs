2037f4d22b236dba3e5e3838cfc2e3daa13fa4e1
boombb12138 <boombb12138@163.com>
2022-10-22 20:53:09 +0800

    fix:change the nft image

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -3,16 +3,17 @@ export default function handler(req, res) {
   const tokenId = req.query.tokenId;
   //   图片都已上传到github 所以可以从github上面提取
   const image_url =
-    "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
+    // "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/";
 
-  // api返回Crypto Dev的元数据
-  //为了使我们的集合与Opensea兼容，我们需要遵循一些元数据标准
-  //返回API的响应
-  res.status(200).json({
-    name: "Crypto Dev #" + tokenId,
-    description: "Crypto Dev is a collection of developers in crypto",
-    image: image_url + tokenId + ".svg",
-  });
+    // api返回Crypto Dev的元数据
+    //为了使我们的集合与Opensea兼容，我们需要遵循一些元数据标准
+    //返回API的响应
+    res.status(200).json({
+      name: "Naomi_Crypto Dev #" + tokenId,
+      description: "Crypto Dev is a collection of developers in crypto",
+      image: require("../../public/yushuitongzuo.jpg"),
+      // image: image_url + tokenId + ".svg",
+    });
 }
 // 在OpenSea上面我们有了一个 API route，可以通过这个 API route取得元数据
 // 具体步骤
